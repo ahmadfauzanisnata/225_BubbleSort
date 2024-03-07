0c51107398616de8334b45409bab38e472ab7286
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-03-07 17:06:31 +0700

    Add project files.

---
+++ project/project.cpp
@@ -0,0 +1,12 @@
+// project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+//
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int arr[20];
+int n;
+
